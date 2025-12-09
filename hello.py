b70a8b5313db5dc62bfa7b9dcf045c6158567509
Lucas <olucasbm1213@gmail.com>
2025-12-09 00:52:35 -0300

    first commit

---
+++ hello.py
@@ -0,0 +1,3 @@
+print("Olá, Mundo!")
+print("Este é meu primeiro programa em Python.")
+print("Vou estudar pelo livro 'Introdução a programação com Python. Algoritmos e lógica de programação para iniciantes do autor Nilo Ney Coutinho Menezes'")
