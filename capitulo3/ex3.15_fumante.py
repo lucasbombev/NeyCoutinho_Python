1c5aae8dfd9b04f75d24c49ae2c9d448aaa8dbcb
Lucas <olucasbm1213@gmail.com>
2025-12-17 21:35:20 -0300

    little coments

--- capitulo3/ex3.15_fumante.py
+++ capitulo3/ex3.15_fumante.py
@@ -1,7 +1,7 @@
 #Escreva um programa para calcular o tempo de vida de um fumante. Pergunte a quantidade de cigarros fumados por dia e quantos anos ele já fumou. Considere que um fumante perde 10 minutos de vida a cada cigarro. Calcule quantos dias de vida um fumante perderá e exiba o total em dias. Considere também que cada cigarro fumado é 1 boquete que o fumante dá no capiroto.
 cigarros_por_dia = int(input("Quantos cigarros você fuma por dia? "))
 anos_fumando = int(input("Há quantos anos você fuma? "))
-
+#const's
 minutos_perdidos_por_cigarro = 10
 dias_no_ano = 365
 
