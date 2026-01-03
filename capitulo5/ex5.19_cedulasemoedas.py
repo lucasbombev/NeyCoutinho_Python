d4b10da3279fc539dc99ee62fc31b79c9f6acd9c
Lucas <olucasbm1213@gmail.com>
2026-01-03 16:22:36 -0300

    thinkpad

---
+++ capitulo5/ex5.19_cedulasemoedas.py
@@ -0,0 +1,27 @@
+valor = float(input("Digite o valor a pagar:"))
+cédulas = 0
+atual = 100
+apagar = valor
+
+moedas = 0
+moeda_atual = 0.50
+while True:
+    if atual <= apagar:
+        apagar -= atual
+        cédulas += 1
+    else:
+        print(f"{cédulas} cédula(s) de R${atual}")
+        print(f"{moedas} moeda(s) de R${moeda_atual}")
+        if apagar == 0:
+            break
+        if atual == 100:
+            atual = 50
+        elif atual == 50:
+            atual = 20
+        elif atual == 20:
+            atual = 10
+        elif atual == 10:
+            atual = 5
+        elif atual == 5:
+            atual = 1
+        cédulas = 0
